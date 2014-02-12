9b5fe8c3e165caaf981675cfd683b2cea5794966
Ed Bueler <elbueler@alaska.edu>
2014-02-11 21:46:43 -0900

    implemented shooting by bisection (works great); observe grounded problem very stiff and floating not so

--- marineshoot.py
+++ marineshoot.py
@@ -2,14 +2,33 @@
 
 ## full marine ice sheet by shooting
 
-#from pylab import *
 import numpy as np
 import matplotlib.pyplot as plt
 from sys import exit
+import argparse
 from scipy.linalg import solve
 from scipy.integrate import odeint
 
 import exactN
+
+desc='''Solve ODE system for marine ice sheet, for which an exact solution is known.
+Either draws a particular case (if '-save foo.png' option given)
+or does shooting to solve for the right value.'''
+
+parser = argparse.ArgumentParser(description=desc)
+
+Tmultdefault = 1.0
+parser.add_argument('-Tmult', default=Tmultdefault,
+                   help='when plotting, multiple of exact value of T to use as initial value (default = %.2f)' % Tmultdefault)
+parser.add_argument('-save', metavar='FILENAME', dest='outfile',
+                   help='if given, save plot in image file; if not given, do shooting with bisection')
+
+args = parser.parse_args()
+Tmult = float(args.Tmult)
+
+doshoot = (args.outfile==None)
+if not doshoot:
+    imagename = str(args.outfile)
 
 afloat = 0.0 * exactN.a   # FIXME: what do I want?
 
@@ -81,32 +100,62 @@ def f(v,x):
   vp = solve(A,g)
   return vp.flatten()
 
+rr = exactN.rho / exactN.rhow
 xinit = 0.1 * exactN.L0
 ux = (1.0/exactN.k) * (2.0 * exactN.H0 / exactN.L0**2)
 uinit = ux * xinit
 Hinit = exactN.H0 * (1.0 - (xinit / exactN.L0)**2.0)
 
-Tcheat = 2.0 * Hinit * B(xinit) * ux**(1.0/exactN.n)  # FIXME: a cheat to avoid shoot; the cheat is to use B()
+def objective(Tinit):
+    v0 = np.array([uinit, Hinit, Tinit])  # initial values at x[0] = xinit
+    x = np.linspace(xinit,1.5 * exactN.xc,2)
+    v = odeint(f,v0,x)                    # solve ODE system to determine v[1,2] = T
+    Tcalvc = 0.5 * exactN.rho * exactN.g * (1.0 - rr) * v[1,1]**2
+    #print "Tinit = %.6e,  (Tfinal - Tcalvc)/Tcalvc = %.6e" % ( Tinit, (v[-1,2]-Tcalvc)/Tcalvc )
+    return (v[1,2] - Tcalvc) / Tcalvc
 
-rr = exactN.rho / exactN.rhow
+Tcheat = 2.0 * Hinit * B(xinit) * ux**(1.0/exactN.n)  # the cheat is to use B()
+
+if doshoot:  # run bisection
+    Tlow = 0.5 * exactN.rho * exactN.g * (1.0 - rr) * 100.0**2
+    Thigh = 0.5 * exactN.rho * exactN.g * (1.0 - rr) * 600.0**2
+    olow = objective(Tlow)
+    ohigh = objective(Thigh)
+    "  [Ta,Tb] = [%.7e,%.7e]  gives  [%2.3e,%2.3e]" % (Tlow,Thigh,olow,ohigh)
+    for j in range(20):
+        Tmid = (Tlow + Thigh) / 2.0
+        omid = objective(Tmid)
+        if omid * olow > 0.0:
+            olow = omid
+            Tlow = Tmid
+        else:
+            ohigh = omid
+            Thigh = Tmid
+        print "  [Ta,Tb] = [%.7e,%.7e]  gives  [%2.3e,%2.3e]    (omid = %10.3e)" \
+            % (Tlow,Thigh,olow,ohigh,omid)
+    print
+    print "compare Tcheat = %.7e" % Tcheat
+    exit(0)
+
+# proceed to make figure if not doshoot
+
 Tcalvg = 0.5 * exactN.rho * exactN.g * (1.0 - rr) * exactN.Hc**2
 
 print "initial conditions:"
 print "  at xinit = %.3f km we have initial values" % (xinit/1000.0)
 print "  u = %7.2f m a-1, H = %.2f m" % (uinit*exactN.secpera, Hinit)
 
-#print "TRYING T = %.6e Pa m" % Tcheat
-#print "COMPARE Tcalvg = %.6e Pa m" % Tcalvg
-
 x = np.linspace(xinit,1.5 * exactN.xc,1001)
 dx = x[1] - x[0]
 
-for Tinit in [0.3*Tcheat, 0.5*Tcheat, 1.0*Tcheat, 1.5*Tcheat, 3.0*Tcheat]:
-  v0 = np.array([uinit, Hinit, Tinit])  # initial values at x[0] = xinit
-  v = odeint(f,v0,x)          # solve ODE system
-  Tcalvc = 0.5 * exactN.rho * exactN.g * (1.0 - rr) * v[-1,1]**2
-  print "Tinit = %.6e,  (Tfinal - Tcalvc)/Tcalvc = %.6e" % ( Tinit, (v[-1,2]-Tcalvc)/Tcalvc )
-exit(0)
+Tinit = Tmult * Tcheat
+print "  and  Tinit = %.6e Pa m = %f * Tcheat  where Tcheat = %.6e" % (Tinit, Tmult, Tcheat)
+
+v0 = np.array([uinit, Hinit, Tinit])  # initial values at x[0] = xinit
+#v, info = odeint(f,v0,x,full_output=True)          # solve ODE system
+v = odeint(f,v0,x)          # solve ODE system
+#Tcalvc = 0.5 * exactN.rho * exactN.g * (1.0 - rr) * v[-1,1]**2
+#print "Tinit = %.6e,  (Tfinal - Tcalvc)/Tcalvc = %.6e" % ( Tinit, (v[-1,2]-Tcalvc)/Tcalvc )
 
 u = v[:,0]
 H = v[:,1]
@@ -159,4 +208,6 @@ plt.ylabel(r"$\beta$(x)   [Pa s m-1]")
 plt.xlabel("x   [km]")
 
 plt.show()
+#plt.savefig(imagename)
+#print '  image file %s saved' % imagename
 
